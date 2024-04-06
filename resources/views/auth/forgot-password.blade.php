34239d4d418bc52d0ab1503886cc632d7b04648f
manjid <imanyazid871@gmail.com>
2024-04-06 21:12:19 +0800

    minor edit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Forgot your password? No problem. Go die. No la just kidding (^_^). Insert your email so that we can send you the reset link.') }}
     </div>
 
     <!-- Session Status -->
@@ -12,14 +12,21 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4 ">
+            <a
+                href="{{ route('login') }}"
+                class="items-center text-gray-400 ml-28 hover:text-gray-900"
+                wire:navigate>
+                back
+            </a>
             <x-primary-button>
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
+
         </div>
     </form>
 </x-guest-layout>
